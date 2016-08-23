5ec8321b75fd3435275f454089bde7ef54f2c512
tobby <tobby@tobby.eu>
2016-08-23 08:26:31 +0200

    Use pointer instead of value

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,11 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "TobbyAPI_Wrapper_Spacenav");
   ros::NodeHandle nh;
-  Spacenav spacenav(&nh);
+  Spacenav* spacenav = new Spacenav(&nh);
   while (ros::ok())
   {
     this_thread::sleep_for(chrono::milliseconds(100));
   }
-
+  delete spacenav;
   return 0;
 }
