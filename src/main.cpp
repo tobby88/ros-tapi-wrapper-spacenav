4cd4f42fb0d85d944d6df68216f075c7e674c8d1
tobby <tobby@tobby.eu>
2016-08-23 06:28:57 +0200

    Connect to API

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "TobbyAPI_Wrapper_Spacenav");
   ros::NodeHandle nh;
   Spacenav spacenav(&nh);
+  spacenav.Connect();
   while (ros::ok())
   {
-    spacenav.Connect();
   }
 
   return 0;
--- src/spacenav.cpp
+++ src/spacenav.cpp
@@ -20,7 +20,37 @@ Spacenav::~Spacenav() {}
 
 // Public member functions
 
-bool Spacenav::Connect() { return true; }
+bool Spacenav::Connect()
+{
+  tobbyapi_msgs::Hello hello;
+  header.stamp = Time::now();
+  header.seq++;
+  hello.request.Header = header;
+  hello.request.Name = "Spacenav";
+  hello.request.UUID = uuid;
+  hello.request.DeviceType = tobbyapi_msgs::HelloRequest::Type_SenderDevice;
+  if (helloClient.call(hello))
+  {
+    if (hello.response.Status == tobbyapi_msgs::Hello::Response::StatusOK)
+    {
+
+      ROS_INFO("Connection established, Status OK, Heartbeat %u",
+               hello.response.Heartbeat);
+      return true;
+    }
+    else
+    {
+      ROS_INFO("Connection error, Heartbeat %u", hello.response.Heartbeat);
+      return false;
+    }
+  }
+  else
+  {
+    ROS_ERROR("Failed to establish connection to hello service");
+    return false;
+  }
+  return false;
+}
 
 // Private member functions
 
@@ -49,6 +79,6 @@ void Spacenav::loadUUID()
     uuidFileOutput << uuid;
     uuidFileOutput.close();
   }
-  ROS_INFO("%s", uuid.c_str());
+  this->uuid = uuid;
   return;
 }
