f9ad267a10e95532358ceb7ff9da98f6344a0d1b
tobby <tobby@tobby.eu>
2016-08-23 06:40:33 +0200

    Try to (re)connect if no connection was established at startup
    Retry it every second on error

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include "ros/ros.h"
 #include "spacenav.hpp"
+#include <chrono>
+#include <thread>
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "TobbyAPI_Wrapper_Spacenav");
   ros::NodeHandle nh;
   Spacenav spacenav(&nh);
-  spacenav.Connect();
+  bool connected = false;
+  while (!connected)
+  {
+    connected = spacenav.Connect();
+    this_thread::sleep_for(chrono::milliseconds(1000));
+  }
   while (ros::ok())
   {
   }
