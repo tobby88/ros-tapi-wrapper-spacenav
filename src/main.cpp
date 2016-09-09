0c289e4fdfc3d473bdef959250fa76705e4c59d6
tobby <tobby@tobby.eu>
2016-09-10 01:06:28 +0200

    Add copyright

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 /******************************************************************************
+*  Copyright (C) 2016 by Tobias Holst                                         *
+*                                                                             *
 *  This file is part of tapi_wrapper_spacenav.                                *
 *                                                                             *
 *  tapi_wrapper_spacenav is free software: you can redistribute it and/or     *
