1c4c9c4153a3dcf16e228799a9152fe1d5c14a64
tobby <tobby@tobby.eu>
2016-09-10 00:54:34 +0200

    Add license informations

--- src/spacenav.cpp
+++ src/spacenav.cpp
@@ -1,3 +1,35 @@
+/******************************************************************************
+*  This file is part of tapi_wrapper_spacenav.                                *
+*                                                                             *
+*  tapi_wrapper_spacenav is free software: you can redistribute it and/or     *
+*  modify it under the terms of the GNU General Public License as published   *
+*  by the Free Software Foundation, either version 3 of the License, or       *
+*  (at your option) any later version.                                        *
+*                                                                             *
+*  tapi_wrapper_spacenav is distributed in the hope that it will be useful,   *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+*  GNU General Public License for more details.                               *
+*                                                                             *
+*  You should have received a copy of the GNU General Public License along    *
+*  with tapi_wrapper_spacenav.  If not, see <http://www.gnu.org/licenses/>.   *
+*                                                                             *
+*  Diese Datei ist Teil von tapi_wrapper_spacenav.                            *
+*                                                                             *
+*  tapi_wrapper_spacenav ist Freie Software: Sie können es unter den          *
+*  Bedingungen der GNU General Public License, wie von der Free Software      *
+*  Foundation Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren      *
+*  veröffentlichten Version, weiterverbreiten und/oder modifizieren.          *
+*                                                                             *
+*  tapi_wrapper_spacenav wird in der Hoffnung, dass es nützlich sein wird,    *
+*  aber OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite    *
+*  Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK  *
+*  Siehe die GNU General Public License für weitere Details.                  *
+*                                                                             *
+*  Sie sollten eine Kopie der GNU General Public License zusammen mit diesem  *
+*  Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>. *
+*******************************************************************************/
+
 #include "spacenav.hpp"
 #include "std_msgs/Bool.h"
 #include "std_msgs/Float64.h"
